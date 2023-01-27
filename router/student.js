0c8302b0d01f51cac2da3a20a9f579a7d7f8bdae
Upeksha1214 <upekshasachintha@gmail.com>
2023-01-27 06:49:50 -0800

    updated

--- router/student.js
+++ router/student.js
@@ -3,6 +3,7 @@ const router=express.Router()
 const Student=require('../module/studentModule')
 
 router.post('/studentAdd',async(req,res)=>{
+    
     try{
         const student= new Student ({
             studentId:req.body.studentId,
@@ -18,6 +19,8 @@ router.post('/studentAdd',async(req,res)=>{
             alResult:req.body.alResult
         })
 
+        console.log(studentId)
+        
         const response = await student.save();
         response != null ? res.json({code:'200',message:'Student create successfull',data:null}) : 
                        res.json({code:'500',message:'Student Create Fail',data:null});
